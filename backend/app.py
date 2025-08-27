f9c13dd17f95caff183ffd0a8e9560a344735a31
XWJWPIY <piy.angrybirds.minecraft@gmail.com>
2025-08-27 22:01:03 +0800

    v0.0.3 test
    Update - Using lines to estimate cost

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import json
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -20,8 +22,10 @@ DISCOUNT_RATES = {
 
 BUS_OPTIONS = [
     "台北市一般公車", "新北市一般公車", "幹線公車", "快速公車",
-    "市民小巴", "內科專車", "跳蛙公車"
+    "市民小巴", "內科專車", "跳蛙公車", "新北市新巴士"
 ]
+
+CITIES = {"taipei": "台北市", "newtaipei": "新北市"}
 
 # 新增的健康檢查 API 端點
 @app.route('/health', methods=['GET'])
@@ -31,14 +35,51 @@ def health_check():
     """
     return jsonify({"status": "ok"}), 200
 
+# 新增的 API 端點，用來載入所有公車路線資料
+@app.route('/api/routes', methods=['GET'])
+def get_routes():
+    """
+    載入所有公車路線資料。
+    """
+    try:
+        file_path = os.path.join(app.root_path, 'data', 'processed', 'all_routes.json')
+        
+        # 檢查檔案是否存在，以避免 FileNotFoundError
+        if not os.path.exists(file_path):
+            return jsonify({"error": "all_routes.json file not found at the specified path"}), 500
+
+        with open(file_path, 'r', encoding='utf-8') as f:
+            all_routes = json.load(f)
+            # print(routes)
+
+        routes = []
+
+        for route in all_routes.keys():
+            temp_line_dict = {"RouteName" : route}
+            temp_line_dict["OutputName"] = route
+            if (all_routes[route]["OtherRouteName"] != None):
+                temp_line_dict["OutputName"] = all_routes[route]["OtherRouteName"] + " " + route
+            routes.append(temp_line_dict)
+
+        # 將所有路線分為「幹線」路線和其他路線
+        trunk_routes = [route for route in routes if "幹線" in route["OutputName"]]
+        trunk_routes.sort(key=lambda x: x["OutputName"])
+        other_routes = [route for route in routes if "幹線" not in route["OutputName"]]
+        
+        # 將「幹線」路線列表放在其他路線列表前面，然後返回合併後的列表
+        sorted_routes = trunk_routes + other_routes
+        # print(sorted_routes)
+        return jsonify(sorted_routes)
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+
 # 車種輸入版票價計算 API
-@app.route('/calculate_fare', methods=['POST'])
-def calculate_fare():
+@app.route('/type_calculate_fare', methods=['POST'])
+def type_calculate_fare():
     try:
         # 從前端接收 JSON 格式的數據
         data = request.get_json()
-
-        # print(data)
 
         fare_type = data.get('fare_type')
         bus_trips = data.get('bus_trips')
@@ -57,7 +98,6 @@ def calculate_fare():
         total_fare = 0
 
         # 遍歷每組搭乘數據並計算總價
-
         # 紀錄前一段車種
         previous_bus_type = None
 
@@ -77,6 +117,66 @@ def calculate_fare():
 
     except Exception as e:
         # 處理任何可能發生的錯誤
+        return jsonify({"error": str(e)}), 500
+    
+@app.route('/line_calculate_fare', methods=['POST'])
+def line_calculate_fare():
+    """
+    接收路線號碼和票種，返回總票價。
+    """
+    try:
+        data = request.get_json()
+        bus_trips = data.get('bus_trips')  # 從前端接收路線名稱列表
+        fare_type = data.get('fare_type', 'full_fare')
+
+        print(data)
+        print(bus_trips)
+        print(fare_type)
+
+
+        # 載入所有路線資料以查詢車種
+        file_path = os.path.join(app.root_path, 'data', 'processed', 'all_routes.json')
+        with open(file_path, 'r', encoding='utf-8') as f:
+            all_routes = json.load(f)
+
+        total_fare = 0
+        previous_bus_type = None
+
+        # 檢查票種是否存在
+        if fare_type not in FARE_RATES:
+            return jsonify({"error": "Invalid fare type"}), 400
+            
+        rate_per_trip = FARE_RATES.get(fare_type)
+        discount_per_trip = DISCOUNT_RATES.get(fare_type)
+
+
+        for trip in bus_trips:
+            trip_count = trip.get('trip_count')
+            line_name = trip.get('line_name')
+
+            route_info = all_routes.get(line_name)
+
+            if not route_info:
+                return jsonify({"error": f"Route '{line_name}' not found"}), 400
+
+            now_bus_type = route_info.get('BusType')
+            if (now_bus_type == "一般公車"):
+                now_bus_type = CITIES.get(route_info.get('City')) + now_bus_type
+
+            if (now_bus_type == "新北市新巴士"):
+                continue
+
+            if not isinstance(trip_count, int) or trip_count <= 0:
+                return jsonify({"error": "Trip count must be a non-negative integer"}), 400
+            
+            # 總費用 = 單段票價 * 搭乘段數
+            total_fare += (rate_per_trip * trip_count)
+            total_fare += (is_get_discount(previous_bus_type, now_bus_type) * discount_per_trip)
+            previous_bus_type = now_bus_type
+
+            
+        return jsonify({"total_fare": total_fare})
+    except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
--- backend/functions/process_routes.py
+++ backend/functions/process_routes.py
@@ -2,13 +2,17 @@ import json
 import os
 
 
-def get_type(merged_data:str, bus_type_map:dict, return_routes:dict, city:str):
+def get_type(merged_data:str, bus_type_map:dict, bus_other_name_map:dict,return_routes:dict, city:str):
     for route in merged_data.get(city).get("BusInfo"):
         route_name = route.get("nameZh")
 
         # print(route_name, end= "  ")
         
         bus_type = "一般公車"
+        other_route_name = None
+
+        if (route_name in bus_other_name_map[city]):
+            other_route_name = bus_other_name_map[city][route_name]
 
         for bus_type_key, bus_type_value in bus_type_map.items():
             if route_name in bus_type_value:
@@ -32,7 +36,8 @@ def get_type(merged_data:str, bus_type_map:dict, return_routes:dict, city:str):
             return_routes[route_name] = {
                 "RouteName": route_name,
                 "BusType": bus_type,
-                "City": city
+                "City": city,
+                "OtherRouteName": other_route_name
             }
     
 
@@ -44,6 +49,7 @@ def process_and_save_routes():
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     merged_file = os.path.join(base_dir, 'data', 'merged', 'merged_bus_routes.json')
     bus_type_file = os.path.join(base_dir, 'data', 'static', 'bus_type_map.json')
+    bus_other_name_file = os.path.join(base_dir, 'data', 'static', 'bus_other_name.json')
     output_dir = os.path.join(base_dir, 'data', 'processed')
     output_file = os.path.join(output_dir, 'all_routes.json')
 
@@ -66,12 +72,18 @@ def process_and_save_routes():
         with open(bus_type_file, 'r', encoding='utf-8') as f:
             bus_type_map = json.load(f)
 
+        if not os.path.exists(bus_other_name_file):
+            print(f"錯誤：找不到車種其他名稱檔案 {bus_other_name_file}")
+            return
+
+        with open(bus_other_name_file, 'r', encoding='utf-8') as f:
+            bus_other_name_map = json.load(f)
 
         # 處理台北市公車資料
-        get_type(merged_data, bus_type_map, all_routes, "taipei")
+        get_type(merged_data, bus_type_map, bus_other_name_map, all_routes, "taipei")
 
         # 處理新北市公車資料
-        get_type(merged_data, bus_type_map, all_routes, "newtaipei")
+        get_type(merged_data, bus_type_map, bus_other_name_map, all_routes, "newtaipei")
 
         # 字元排序
         all_routes = dict(sorted(list(all_routes.items())))
